27d4799682344b01ec96b5dc61f760700778d124
arnstein87 <arnstein87@f083f08b-3b3d-0410-9e6b-a8463f5da112>
2008-01-24 09:29:23 +0000

    r1411@laptop:  arnold | 2008-01-24 13:03:18 +0530  gah.. finally decided on C++, so here is my first converted file.
    git-svn-id: https://rediffbol-prpl.svn.sourceforge.net/svnroot/rediffbol-prpl/trunk@43 f083f08b-3b3d-0410-9e6b-a8463f5da112

---
+++ messagebuffer.cpp
@@ -0,0 +1,96 @@
+
+#include "messagebuffer.h" 
+#include "string"
+
+using namespace std; 
+MessageBuffer::MessageBuffer(string s) { 
+	this->str = s;
+	this->offset = 0;
+	this->err = 0 ; 
+}
+
+int 
+MessageBuffer::left() { 
+	return str.length() - offset ; 
+}
+
+
+MessageBuffer::~MessageBuffer() { 
+}
+
+gint32 
+MessageBuffer::readInt32() {
+	if ( this->left() < 4 ) { 
+		throw MessageBufferOverflowException() ;
+	}
+	int i = *((gint32*) (this->str.c_str()+this->offset));
+	this->offset += 4; 
+	return i ;
+}
+
+void 
+MessageBuffer::seek(int len) { 
+	if ( this->offset + len >= this->str.length()) 
+		throw MessageBufferOverflowException() ;
+	this->offset += len ;
+}
+
+char 
+MessageBuffer::readByte() {
+	if ( this->offset >= this->str.length()) { 
+		throw MessageBufferOverflowException() ;
+	}
+	return str[this->offset++] ;
+}
+
+string
+MessageBuffer::readBytes(int len) { 
+	int i ;
+	if ( this->left() < len ) { 
+		throw MessageBufferOverflowException() ;
+	}
+	string s ; 
+	for(i = 0 ; i < len; i++) 
+		s+= readByte() ;
+	return s;
+}
+
+MessageBuffer
+MessageBuffer::readMessageBuffer(int len){ 
+	return MessageBuffer(readBytes(len)) ;
+}
+
+string
+MessageBuffer::readString() { 
+	int len = readInt32() ;
+	return readBytes(len) ;
+}
+
+string
+MessageBuffer::readStringn(int len) { 
+	if ( len == 0 ) return "" ;
+	return readBytes(len) ;
+}
+
+gint16 
+MessageBuffer::readShort() { 
+	gint16 ret = readByte() ;
+	ret = (((gint16)readByte()) << 8 ) | ret ;
+	return ret ;
+}
+
+gboolean 
+MessageBuffer::isEnd() { 
+	return ( offset >= str.length()) ;
+}
+
+gboolean 
+MessageBuffer::getLength() { 
+	return (str.length()) ;
+}
+
+void 
+MessageBuffer::reset() {
+	offset = 0 ;
+	err = 0 ;
+}
--- messagebuffer.h
+++ messagebuffer.h
@@ -1,34 +1,47 @@
+#ifndef _MESSAGEBUFFER_H_
+#define _MESSAGEBUFFER_H_
 
 #include <glib.h>
+#include <string>
+class MessageBufferOverflowException { 
+public:
+};
 
-struct MessageBuffer { 
+class MessageBuffer { 
+
+private:
 	int offset ; 
 	int err ; 
-	GString * str ; 
+	std::string str ; 
+
+public:
+	MessageBuffer (std::string s) ;
+	~MessageBuffer() ;
+
+	gint32 readInt32();
+
+	int left();
+	void seek(int len) ;
+
+	char readByte() ;
+
+	std::string readBytes( int len) ;
+
+	MessageBuffer
+	readMessageBuffer( int len);
+
+	std::string readString() ;
+
+	std::string readStringn(int len) ;
+
+	gint16 readShort() ;
+
+	gboolean isEnd() ;
+
+	gboolean getLength() ; 
+
+	void reset() ;
+
 } ;
 
-extern struct MessageBuffer *messagebuffer_init(const gchar *s) ;
-extern void messagebuffer_free(struct MessageBuffer* m) ;
-
-extern gint32 messagebuffer_readInt32(struct MessageBuffer *m);
-
-extern void messagebuffer_seek(struct MessageBuffer *m, int len) ;
-
-extern char messagebuffer_readByte(struct MessageBuffer *m) ;
-
-extern void messagebuffer_readBytes(struct MessageBuffer *m, GString *s, int len) ;
-
-extern struct MessageBuffer* 
-messagebuffer_readMessageBuffer(struct MessageBuffer *m,  int len);
-
-extern GString* messagebuffer_readString(struct MessageBuffer *m) ;
-
-extern GString* messagebuffer_readStringn(struct MessageBuffer*m, int len) ;
-
-extern gint16 messagebuffer_readShort(struct MessageBuffer *m) ;
-
-extern gboolean messagebuffer_isEnd(struct MessageBuffer *m) ;
-
-extern gboolean messagebuffer_getLength(struct MessageBuffer *m) ; 
-
-extern void messagebuffer_reset(struct MessageBuffer *m) ;
+#endif
