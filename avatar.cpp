1e898026c057b110ccdf46ff3680873b451725b6
arnstein87 <arnstein87@f083f08b-3b3d-0410-9e6b-a8463f5da112>
2008-01-31 16:39:25 +0000

    r1500@laptop:  arnold | 2008-01-31 14:57:06 +0530  oops.. forgot to add the avatar file.
    git-svn-id: https://rediffbol-prpl.svn.sourceforge.net/svnroot/rediffbol-prpl/trunk@89 f083f08b-3b3d-0410-9e6b-a8463f5da112

---
+++ avatar.cpp
@@ -0,0 +1,101 @@
+
+
+#include "rediffbol.h"
+#include <util.h>
+
+using namespace std; 
+using namespace rbol ; 
+
+static void load_avatar_callback(PurpleUtilFetchUrlData* url_data, 
+				  gpointer user_data, const gchar * url_text, 
+				  gsize len ,const gchar* error_message) {
+	purple_debug_info ("rbol", "avatar callback\n");
+	pair<RediffBolConn*, string> *data = NULL; 
+	data = (typeof(data)) ( user_data ) ;
+
+	if ( url_text == NULL or data ->first->isInvalid()  )  {
+		delete data ;
+		return  ; 
+	}
+	data->first->_loadAvatarCompleted(data->second, string(url_text, 
+							       url_text+len));
+	delete data;
+}
+
+
+static void load_avatar_key_callback(PurpleUtilFetchUrlData* url_data, 
+				  gpointer user_data, const gchar * url_text, 
+				  gsize len ,const gchar* error_message) {
+	pair<RediffBolConn*, string> *data = NULL; 
+	data = (typeof(data)) ( user_data ) ;
+
+	if ( ! url_text ) { 
+		purple_debug_info("rbol", "Unable to load url string\n") ;
+		delete data ; 
+	}
+
+	purple_debug_info("rbol", "Key String: %s\n", url_text) ;
+	string name = data->second ;
+
+	string url = "http://imavatars.rediff.com/avatars/getmasque_new.asp?type=image&username=" + name + "&key="; 
+	
+	string r_key  (url_text, url_text+len) ;
+	r_key = r_key.substr(5) ;
+
+	for(int l = 0; l < r_key.size(); l++) 
+		if ( r_key[l] == '<' ) { 
+			r_key = r_key.substr(0, l) ;
+			break ;
+		}
+	
+
+	url += r_key ;
+
+
+	purple_debug_info("rbol", "Ok, getting avatar for %s\n", name.c_str());
+	purple_util_fetch_url_request(strdup(url.c_str()), 
+				      true, 
+				      NULL, /* useragent */
+				      true,  /* http1.1*/
+				      NULL, /* request */
+				      false, /* don't include headers */
+				      load_avatar_callback, 
+				      (void*) data );
+
+}
+void RediffBolConn::loadAvatar(std::string name) { 
+	pair<RediffBolConn*, string> *data = new pair<RediffBolConn*, string> 
+		(this, name) ;
+
+	string url = "http://avatars.rediff.com/avatars/getkey.asp?username="
+		+ name + 
+		+ "&secret=rediffbol&login=" 
+	        + getServerUserId() + "&session_id="
+		+ getSessionString();
+
+
+	purple_util_fetch_url_request(strdup(url.c_str()), 
+				      true, 
+				      NULL, /* useragent */
+				      true,  /* http1.1*/
+				      NULL, /* request */
+				      false, /* don't include headers */
+				      load_avatar_key_callback, 
+				      (void*) data );
+	
+
+
+}
+
+void RediffBolConn::_loadAvatarCompleted(std::string name, 
+					 std::string avatar_data) { 
+	assert(!isInvalid()) ;
+
+	char * data = new char[avatar_data.length()] ; 
+	memcpy(data, (void*) avatar_data.data(), avatar_data.length());
+	purple_buddy_icons_set_for_user(account,
+					name.c_str(), 
+					(void*) data, 
+					avatar_data.length(), 
+					NULL);
+}
