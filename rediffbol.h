b18868099d6f6c7dfde7890b2601346d0f048168
arnstein87 <arnstein87@f083f08b-3b3d-0410-9e6b-a8463f5da112>
2008-02-04 10:15:35 +0000

    r1536@laptop:  arnold | 2008-02-04 13:38:43 +0530  reverting useragent to what it was for the timebeing.
    git-svn-id: https://rediffbol-prpl.svn.sourceforge.net/svnroot/rediffbol-prpl/trunk@107 f083f08b-3b3d-0410-9e6b-a8463f5da112

--- rediffbol.h
+++ rediffbol.h
@@ -40,8 +40,8 @@ namespace rbol {
 	#define  CAP_DIR  "RBOL/1.2.5" 
         #define  CAP_HTTP_CONNECT  "RBOL/1.2.5+HTTP_CONNECT" 
         #define  CAP_HTTP  "RBOL/1.2.5+HTTP" 
-//	#define  DEFAULT_USERAGENT  "Rediff Bol8.0 build 315" 
-	#define  DEFAULT_USERAGENT  "libpurple:rediffbol-prpl" 
+	#define  DEFAULT_USERAGENT  "Rediff Bol8.0 build 315" 
+//	#define  DEFAULT_USERAGENT  "libpurple:rediffbol-prpl" 
 	extern void hex_dump (const std::string a,const std::string message) ;
 	
 	class PurpleAsyncConn ;
