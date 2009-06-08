54ae7f7b3236ef024867e20e6afc290626e48278
Arnold Noronha <arnold@arnosh.(none)>
2009-06-07 21:21:50 -0400

    Added copyright notice to each file.

--- rediffbol_init.cpp
+++ rediffbol_init.cpp
@@ -1,6 +1,7 @@
 /**
- * rediffbol.c
- * (c) Copyright 2007, Arnold Noronha
+ * @file rediffbol.c
+ * 
+ * Copyright 2007-2008 Arnold Noronha <arnstein87 AT gmail DOT com>
  * 
  * This program is free software;you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
